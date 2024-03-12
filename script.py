bda0835cff7437ff5cd2eb71479d6394b228fd83
lucas vincent <95697387+Croooze@users.noreply.github.com>
2024-03-12 15:36:35 +0100

    Création des dossiers et fichiers pour html et css

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/index/')
+@app.route('/index/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
